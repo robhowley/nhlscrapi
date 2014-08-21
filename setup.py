06451bdb55faaa7fd22f7bac403d00dda0018c5d
robhowley <howley.robert@gmail.com>
2014-08-21 18:48:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="nhlscrapi",
+    
+    version=nhlscrapi.__version__,
+    
+    description='NHL Scrapr API for Python',
+    
+    author='Rob Howley',
+    author_email='howley.robert@gmail.com',
+    url='https://github.com/robhowley/nhlscrapi',
+    
+    packages=find_packages(),
+    
+    include_package_data=True,
+    
+    license="Apache Software License version 2.0",
+    
+    platforms='any',
+    
+    zip_safe=False,
+    
+    keywords='nhlscrapi',
+    
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+    test_suite='tests',
+    
+    # Dependent packages (distributions)
+    install_requires=[],
+)
