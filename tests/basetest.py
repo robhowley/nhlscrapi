6901b95d987d0e965e852523dee9e37f9cf4047d
robhowley <howley.robert@gmail.com>
2014-08-21 19:46:04 -0400

    placeholder for base rtss parse test

---
+++ tests/basetest.py
@@ -0,0 +1,17 @@
+import unittest
+
+# TODO: load a game, parse events, assert final score
+
+class TestRTSSParse(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def test_game(self):
+    pass
+    
+  def tearDown(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
