59bdf8ac6354c7ece7355e6325cb20f6f1d2d150
robhowley <howley.robert@gmail.com>
2015-07-29 23:15:11 -0400

    extending face-off comp interface; totals, by zone, win %; updated fo tests

--- nhlscrapi/games/faceoffcomp.py
+++ nhlscrapi/games/faceoffcomp.py
@@ -4,26 +4,156 @@ from nhlscrapi.games.repscrwrap import RepScrWrap, dispatch_loader
 
 
 class FaceOffComparison(RepScrWrap):
+    """
+    Face-off Comparison and summary report. Produces the by player and head-to-head matchup face off totals
+    by zone and overall. For either home or away the full summary data takes the following form
+        {
+            'home/away': {
+                player_num: {
+                    'off/def/neut/all': { 'won': won, 'total': total }
+                }
+            }
+        }
+    """
+    
     def __init__(self, game_key):
         super(FaceOffComparison, self).__init__(game_key, FaceOffRep(game_key))
+        self.__team_tots = None
+        self.__zones = [ 'off', 'def', 'neut', 'all' ]
         
     @property
     @dispatch_loader('_rep_reader', 'parse_home_face_offs')
     def home_fo(self):
+        """
+        Property that returns the full by player face-off summary for the home team. See FaceOffComparison
+        for dictionary key structure.
+        
+        :returns: dict
+        """
         return self._rep_reader.face_offs['home']
         
     @property
     @dispatch_loader('_rep_reader', 'parse_away_face_offs')
     def away_fo(self):
+        """
+        Property that returns the full by player face-off summary for the away team. See FaceOffComparison
+        for dictionary key structure.
+        
+        :returns: dict
+        """
         return self._rep_reader.face_offs['away']
     
-    @dispatch_loader('_rep_reader', 'parse_away_face_offs')
-    @dispatch_loader('_rep_reader', 'parse_home_face_offs')
     def head_to_head(self, home_num, away_num):
-        h_fo = self._rep_reader.face_offs['home'][home_num]['opps'][away_num]
-        a_fo = self._rep_reader.face_offs['away'][away_num]['opps'][home_num]
-        zones = [ 'off', 'def', 'neut', 'all' ]
+        """
+        Return the head-to-head face-off outcomes between two players.
+        If the matchup happened, then the format of the output is
+            {
+                'home': { 'off/def/neut/all': { 'won': won, 'total': total } }
+                'away': { 'off/def/neut/all': { 'won': won, 'total': total } }
+            }
+        else an empty { } dictionary will be returned
+        
+        :param home_num: the number of the home team player
+        :param away_num: the number of the away team player
+        :returns: dict described above or { }
+        """
+        h_fo = self.home_fo[home_num]['opps'][away_num]
+        a_fo = self.away_fo[away_num]['opps'][home_num]
         return {
-            'home': { k: h_fo[k] for k in zones },
-            'away': { k: a_fo[k] for k in zones }
+            'home': { k: h_fo[k] for k in self.__zones },
+            'away': { k: a_fo[k] for k in self.__zones }
+        }
+
+    @property
+    def team_totals(self):
+        """
+        Returns the overall faceoff win/total breakdown for home and away as
+            {
+                'home/away': { 'won': won, 'total': total }
+            }
+        
+        :returns: dict
+        """
+        if self.__team_tots is None:
+            self.__team_tots = self.__comp_tot()
+        
+        return {
+            t: self.__team_tots[t]['all']
+            for t in [ 'home', 'away' ]
+        }
+
+    @property
+    def by_zone(self):
+        """
+        Returns the faceoff win/total breakdown by zone for home and away as
+            {
+                'home/away': {
+                    'off/def/neut': { 'won': won, 'total': total }
+                }
+            }
+        
+        :returns: dict
+        """
+        if self.__team_tots is None:
+            self.__team_tots = self.__comp_tot()
+        
+        return {
+            t: {
+                z: self.__team_tots[t][z]
+                for z in self.__zones
+                if z != 'all'
+            }
+            for t in [ 'home', 'away' ]
+        }
+       
+    @property
+    def fo_pct(self):
+        """
+        Get the by team overall face-off win %. Format is { 'home': %, 'away': % }
+        
+        :returns: dict
+        """
+        tots = self.team_totals
+        return {
+            t: tots[t]['won']/(1.0*tots[t]['total']) if tots[t]['total'] else 0.0
+            for t in [ 'home', 'away' ]
+        }
+        
+    @property
+    def fo_pct_by_zone(self):
+        """
+        Get the by team face-off win % by zone. Format is
+            {
+                'home/away': { 'off/def/neut': % }
+            }
+            
+        :returns: dict
+        """
+        bz = self.by_zone
+        return {
+            t: {
+                z: bz[t][z]['won']/(1.0*bz[t][z]['total']) if bz[t][z]['total'] else 0.0
+                for z in self.__zones
+                if z != 'all'
+            }
+            for t in [ 'home', 'away' ]
+            
+        }
+        
+    def __comp_tot(self):
+        def tbz(fos):
+            r = {
+                z: { 'won': 0, 'total': 0 }
+                for z in self.__zones
+            }
+            
+            for _, d in fos.items():
+                for z in self.__zones:
+                    r[z]['won'] += d[z]['won']
+                    r[z]['total'] += d[z]['total']
+            return r
+            
+        return {
+            'home': tbz(self.home_fo),
+            'away': tbz(self.away_fo)
         }
--- tests/test_faceoffcomp.py
+++ tests/test_faceoffcomp.py
@@ -1,8 +1,6 @@
 import unittest
 
-# TODO: load a game, parse events, assert final score
-
-class TestRTSSParse(unittest.TestCase):
+class TestFaceOffComparison(unittest.TestCase):
 
     def test_face_off_comparison(self):
         from nhlscrapi.games.gamekey import GameKey
@@ -25,5 +23,22 @@ class TestRTSSParse(unittest.TestCase):
             
         # equivalently, stepan 0/1 in offensive
         self.assertEqual(hth['away']['off'], { 'won': 0, 'total': 1 })
-
         
+        # face off win %
+        rnd = { k: round(v, 2) for k, v in foc.fo_pct.items() }
+        self.assertEqual(rnd, { 'home': 0.57, 'away': 0.43 })
+        
+        # neut zone face off records
+        self.assertEqual(foc.by_zone['away']['neut'], { 'won': 10, 'total': 24 })
+        self.assertEqual(foc.by_zone['home']['neut'], { 'won': 14, 'total': 24 })
+        
+        # neut zone face off %
+        self.assertEqual(round(foc.fo_pct_by_zone['away']['neut'],2), 0.42)
+        
+        # away off/home def zone face off records
+        self.assertEqual(foc.by_zone['away']['off'], { 'won': 9, 'total': 23 })
+        self.assertEqual(foc.by_zone['home']['def'], { 'won': 14, 'total': 23 })
+
+
+if __name__ == '__main__':
+    unittest.main()
