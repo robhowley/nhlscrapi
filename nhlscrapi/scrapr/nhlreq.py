2f75a2e91f58159c82a57b8768050adb6f8f8755
robhowley <howley.robert@gmail.com>
2016-01-10 13:59:29 -0500

    Closes #15. Stupid indentation error

--- nhlscrapi/scrapr/nhlreq.py
+++ nhlscrapi/scrapr/nhlreq.py
@@ -7,85 +7,85 @@ class NHLCn(object):
     an error occurs in the retrieval process, the error is recorded in ``req_err``.
     """
     __domain = 'http://www.nhl.com/'
-    
+
     def __init__(self):
         self.html_src = None
         """The HTML source of the last NHL page requested."""
         self.req_err = None
         """Error from the last HTTP request."""
-        
+
     def __html_rep(self, game_key, rep_code):
         """Retrieves the nhl html reports for the specified game and report code"""
         seas, gt, num = game_key.to_tuple()
         url = [ self.__domain, "scores/htmlreports/", str(seas-1), str(seas),
                 "/", rep_code, "0", str(gt), ("%04i" % (num)), ".HTM" ]
         url = ''.join(url)
-        
+
         return self.__open(url)
-    
+
     def game_roster(self, game_key):
         """
         :returns: raw HTML for game rosters (RO)
         :rtype: string
         """
         return self.__html_rep(game_key, 'RO')
-        
+
     def rtss(self, game_key):
         """
         :returns: raw HTML for RTSS play by play (PL)
         :rtype: string
         """
         return self.__html_rep(game_key, 'PL')
-        
+
     def home_toi(self, game_key):
         """
         :returns: raw HTML for home TOI by player (TH)
         :rtype: string
         """
         return self.__html_rep(game_key, 'TH')
-        
+
     def away_toi(self, game_key):
         """
         :returns: raw HTML for away TOI by player (TV)
         :rtype: string
         """
         return self.__html_rep(game_key, 'TV')
-        
+
     def face_offs(self, game_key):
         """
         :returns: raw HTML for face off comparisons (FC)
         :rtype: string
         """
         return self.__html_rep(game_key, 'FC')
-        
+
     def shootout(self, game_key):
         """
         :returns: raw HTML for the game's shootout (SO)
         :rtype: string
         """
         return self.__html_rep(game_key, 'SO')
-        
+
     def game_summary(self, game_key):
         """
         :returns: raw HTML for game summary report (GS)
         :rtype: string
         """
         return self.__html_rep(game_key, 'GS')
-        
+
     def event_summary(self, game_key):
         """
         :returns: raw HTML for the event summary report (ES)
         :rtype: string
         """
         return self.__html_rep(game_key, 'ES')
-        
+
     def shot_summary(self, game_key):
         """
         :returns: raw HTML for the shot summary report (SS)
         :rtype: string
         """
         return self.__html_rep(game_key, 'SS')
-        
+
     def __open(self, url):
         req = None
         try:
@@ -100,6 +100,5 @@ class NHLCn(object):
             self.html_src = req.text
         except Exception as e:
             self.req_err = e
-                
-        return self.html_src
 
+        return self.html_src
--- nhlscrapi/scrapr/rosterrep.py
+++ nhlscrapi/scrapr/rosterrep.py
@@ -7,69 +7,68 @@ from nhlscrapi.scrapr.reportloader import ReportLoader
 
 class RosterRep(ReportLoader):
     """Retrieve and load roster report from nhl.com. This is a base class and not intended to be used directly."""
-    
+
     def __init__(self, game_key):
         super(RosterRep, self).__init__(game_key, 'game_roster')
-        
+
         self.rosters = { }
         """
         Rosters for both home and away.
         Data: ``{ 'home/away': { num: { 'pos': '', 'name': '' } } }``
         """
-        
+
         self.scratches = { }
         """Dictionary of healthy scratches keyed home/away loaded"""
-        
+
         self.coaches = { }
         """Dictionary of coaches keyed home/away loaded"""
-        
+
         self.officials = { 'refs': { }, 'linesman': { } }
         """Game officials: ``{ 'refs': { }, 'linesman': { } }``"""
-        
+
         self.__blocks = {}
-        
-        
+
+
     def __pl_blocks(self, doc):
         bls = doc.xpath('//td[text()="#"]')
-        
+
         # roster page layout
         # AWAY ROSTER  | HOME ROSTER
         # AWAY SCRATCH | HOME SCRATCH
         bl_k = { 1: 'away', 2: 'home', 3: 'aw_scr', 4: 'h_scr' }
-        
+
         # clean blocks
         for i, bl in enumerate(bls):
             table = bl.xpath('../..')[0]
             self.__blocks[bl_k[i+1]] = table
-            
-            
+
+
     def __clean_pl_block(self, bl):
         def no_letter(s):
             s = s.strip()
             return '(C)' not in s and '(A)' not in s
-            
+
         r = { }
-        
         for p in bl:
             txt = p.xpath('.//text()')
             if len(txt) and '#' not in txt[0]:
                 txt = ex_junk(txt, ['\r','\n'])
                 txt[2] = ' '.join(s.strip() for s in txt[2].split(' ') if no_letter(s))
-                
-            # need some unique key
-            num = int(txt[0]) if txt[0].isdigit() else max(r.keys())+1
-            r[num] = { 'position': txt[1], 'name': txt[2] }
-            
+
+                # need some unique key
+                num = int(txt[0]) if txt[0].isdigit() else len(r.keys())
+                r[num] = { 'position': txt[1], 'name': txt[2] }
+
         return r
-        
-        
+
+
     def parse(self):
         """
         Retreive and parse Play by Play data for the given :py:class:`nhlscrapi.games.game.GameKey``
-        
+
         :returns: ``self`` on success, ``None`` otherwise
         """
-        
+
         try:
             return super(RosterRep, self).parse() \
                 .parse_rosters() \
@@ -78,69 +77,69 @@ class RosterRep(ReportLoader):
                 .parse_officials()
         except:
             return None
-            
-            
+
+
     def parse_rosters(self):
         """
         Parse the home and away game rosters
-        
+
         :returns: ``self`` on success, ``None`` otherwise
         """
         lx_doc = self.html_doc()
-        
+
         if not self.__blocks:
             self.__pl_blocks(lx_doc)
-            
+
         for t in ['home', 'away']:
             self.rosters[t] = self.__clean_pl_block(self.__blocks[t])
-            
+
         return self if self.rosters else None
-        
-        
+
+
     def parse_scratches(self):
         """
         Parse the home and away healthy scratches
-        
+
         :returns: ``self`` on success, ``None`` otherwise
         """
         lx_doc = self.html_doc()
         if not self.__blocks:
             self.__pl_blocks(lx_doc)
-            
+
         for t in ['aw_scr', 'h_scr']:
             ix = 'away' if t == 'aw_scr' else 'home'
             self.scratches[ix] = self.__clean_pl_block(self.__blocks[t])
-            
+
         return self if self.scratches else None
-        
-        
+
+
     def parse_coaches(self):
         """
         Parse the home and away coaches
-        
+
         :returns: ``self`` on success, ``None`` otherwise
         """
         lx_doc = self.html_doc()
         tr = lx_doc.xpath('//tr[@id="HeadCoaches"]')[0]
-        
+
         for i, td in enumerate(tr):
             txt = td.xpath('.//text()')
             txt = ex_junk(txt, ['\n','\r'])
             team = 'away' if i == 0 else 'home'
             self.coaches[team] = txt[0]
-            
+
         return self if self.coaches else None
-        
-        
+
+
     def parse_officials(self):
         """
         Parse the officials
-        
+
         :returns: ``self`` on success, ``None`` otherwise
         """
         # begin proper body of method
         lx_doc = self.html_doc()
         off_parser = opm(self.game_key.season)
         self.officials = off_parser(lx_doc)
-        
+
         return self if self.officials else None
